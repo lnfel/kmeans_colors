ad53ae1ef9d421b86b4e015bc4fc8cea374b5ed3
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-03-25 13:47:15 +0800

    Benchmark kmeansColors loop

--- src/routes/api/kmeans_colors/+server.js
+++ src/routes/api/kmeans_colors/+server.js
@@ -6,6 +6,7 @@ import { kmeansColors } from "../../../lib/ghostprinter/print-job/pdf.js"
  * Handle Post request to /kmeans_colors
  * 
  * https://kit.svelte.dev/docs/routing#server
+ * https://dev.to/imichaelowolabi/this-is-why-your-nodejs-application-is-slow-206j
  * 
  * @returns {Response} Response object
  */
@@ -18,11 +19,34 @@ export const POST = async ({ request, locals, cookies }) => {
     const filedir = await saveFromBase64(base64Images)
 
     let kmeans_colors = []
+
+    /**
+     * The Promise optimization is not applicable since running all promises
+     * in parallel results in processing images beyond page 1, which results
+     * in timing issue with the following error:
+     * 
+     * Format error decoding Png: Unexpected end of data before image end.
+     */
+    // const before = Date.now()
+    // const kmeansColorsPromise = base64Images.map((image, index) => {
+    //     const imagepath = `${filedir}/page-${index}.png`
+    //     return kmeansColors(imagepath).then((color) => {
+    //         return color
+    //     })
+    // })
+
+    // const kmeans_colors = await Promise.all(kmeansColorsPromise)
+    // const after = Date.now()
+    // console.log(`kmeansColors Promise all done in ${(after - before) / 1000} s`)
+
+    const before = Date.now()
     for (let i = 0; i < images.length; i++) {
         const imagepath = `${filedir}/page-${i}.png`
         const color = await kmeansColors(imagepath)
         kmeans_colors = [...kmeans_colors, color]
     }
+    const after = Date.now()
+    console.log(`kmeansColors loop done in ${(after - before) / 1000} s`)
 
     return json({
         kmeans_colors
