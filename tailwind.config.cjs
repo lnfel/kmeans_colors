b4d19731563d10166e917e9b4fb9ac05165f4236
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-04-12 13:03:00 +0800

    Add wisteria color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                /**
+                 * https://uicolors.app/create
+                 * #9364b9
+                 */
+                'wisteria': {
+                    '50': '#f4f2fb',
+                    '100': '#ece8f7',
+                    '200': '#ddd5f0',
+                    '300': '#c9bbe6',
+                    '400': '#b79fda',
+                    '500': '#a886ce',
+                    '600': '#9364b9',
+                    '700': '#875ca6',
+                    '800': '#6c4d86',
+                    '900': '#58436c',
+                    '950': '#34273f',
+                },                
+            }
+        },
     },
     plugins: [],
 }
