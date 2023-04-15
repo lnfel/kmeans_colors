ad182bace54b75cabb7e362e0e64f442bed96d9d
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-04-15 13:48:57 +0800

    [Feature] Google drive docx uploads to Aerial folder

--- src/routes/api/google/+server.js
+++ src/routes/api/google/+server.js
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit'
 import { getOAuth2Client, isSignedIn } from 'svelte-google-auth'
-import { google } from 'googleapis'
+import { google, drive_v3, Auth } from 'googleapis'
 
 /**
  * https://developers.google.com/drive/api/guides/manage-uploads
@@ -12,6 +12,10 @@ import { google } from 'googleapis'
  * https://stackoverflow.com/questions/73259716/how-to-solve-failed-to-parse-content-range-header-in-google-drive-api-for-perf
  * https://stackoverflow.com/questions/75569067/google-api-driveactivity-query-gives-insufficient-scope-error
  * https://stackoverflow.com/questions/60027518/how-to-correctly-read-pdf-into-nodejs-buffer-from-google-drive-apis-export-meth
+ * https://stackoverflow.com/questions/71835232/how-do-i-do-a-resumable-upload-to-a-shared-folder-in-python-using-google-drive-a
+ * 
+ * Huge credits to Tanaike
+ * https://stackoverflow.com/users/7108653/tanaike
  */
 export const POST = async ({ request, locals, cookies, fetch }) => {
     const file = await request.json()
@@ -36,25 +40,26 @@ export const POST = async ({ request, locals, cookies, fetch }) => {
         console.log('client: ', client)
 
         try {
-            // Perform resumable upload, initial request
-            const resumable = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable', {
-                method: 'POST',
-                headers: {
-                    "Authorization": `Bearer ${client.credentials.access_token}`,
-                    "X-Upload-Content-Type": file.type,
-                    "X-Upload-Content-Length": file.size,
-                    "Content-Type": "application/json; charset=UTF-8"
-                },
-                body: JSON.stringify({
-                    name: file.name,
-                    // mimeType: file.type
-                    // explicitly assign google workspace document mimeType so we can perform
-                    // export operations with conversions
-                    mimeType: 'application/vnd.google-apps.document'
-                })
+            const aerialFolder = await searchAerialFolder(client) ?? await aerialFolderCreate(client)
+            console.log(aerialFolder)
+
+            const resumableHeaders = {
+                "Authorization": `Bearer ${client.credentials.access_token}`,
+                "X-Upload-Content-Type": file.type,
+                "X-Upload-Content-Length": file.size,
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+            const resumableBody = JSON.stringify({
+                name: file.name,
+                // mimeType: file.type
+                // explicitly assign google workspace document mimeType so we can perform
+                // export operations with conversions
+                mimeType: 'application/vnd.google-apps.document',
+                parents: [aerialFolder.id]
             })
 
-            // console.log('resumable headers: ', resumable.headers)
+            // Perform resumable upload, initial request
+            const resumable = await startResumableUpload(resumableHeaders, resumableBody)
             console.log('resumable headers location: ', resumable.headers.get('location'))
 
             // Perform resumable upload, second part
@@ -102,4 +107,63 @@ export const POST = async ({ request, locals, cookies, fetch }) => {
     return json({
         message: 'Please sign in using google oauth first.'
     })
+}
+
+/**
+ * Initiate resumable upload by sending initial request with query parameter of uploadType=resumable
+ * 
+ * If the initiation request succeeds, the response includes a 200 OK HTTP status code. In addition, it includes a Location header that specifies the resumable session URI.
+ * 
+ * Save the resumable session URI so you can upload the file data and query the upload status. A resumable session URI expires after one week.
+ * 
+ * @param {[string, string][] | Record<string, string> | Headers} headers Request Headers
+ * @param {ReadableStream | XMLHttpRequestBodyInit} body Request Body
+ * @returns {Promise<Response>}
+ */
+async function startResumableUpload(headers, body) {
+    return await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable', {
+        method: 'POST',
+        headers,
+        body
+    })
+}
+
+/**
+ * Look for Aerial folder in current user's google drive
+ * 
+ * https://github.com/googleapis/google-api-nodejs-client/issues/2208#issuecomment-671676734
+ * 
+ * @param {Auth.OAuth2Client} client OAuth2Client
+ * @returns {Promise<drive_v3.Schema$File|undefined>} Returns drive file object when found and undefined when not found
+ */
+async function searchAerialFolder(client) {
+    const drive = google.drive({version: 'v3', auth: client})
+    const listResponse = await drive.files.list({
+        q: "mimeType='application/vnd.google-apps.folder' and trashed=false",
+        fields: 'files(id,name)',
+    })
+
+    return listResponse.data.files.find((folder) => folder.name === 'Aerial')
+}
+
+/**
+ * Create Aerial folder in current user's google drive
+ * 
+ * @param {Auth.OAuth2Client} client OAuth2Client
+ * @returns {Promise<drive_v3.Schema$File>} Returns drive file object
+ */
+async function aerialFolderCreate(client) {
+    const drive = google.drive({version: 'v3', auth: client})
+    const fileMetadata = {
+        name: 'Aerial',
+        mimeType: 'application/vnd.google-apps.folder',
+    }
+
+    const folderResponse = await drive.files.create({
+        requestBody: fileMetadata,
+        // fields: 'id,name,kind,mimeType',
+        fields: 'id,name',
+    })
+
+    return folderResponse.data
 }
