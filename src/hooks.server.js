e84af50b4824b80303d2e43d42430b7340013a67
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-04-17 11:57:25 +0800

    [Revert] Stop initiating cron job within sveltekit hooks, causes increase in CPU usage

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,15 +1,17 @@
 import { SvelteGoogleAuthHook } from 'svelte-google-auth/server'
 import client_secret from '../client_secret.json'
-import { cleanStorage, cleanLogs, cleanCronLogs } from '$lib/aerial/server/cron'
+// import { cleanStorage, cleanLogs, cleanCronLogs } from '$lib/aerial/server/cron.js'
 
 /**
  * On dev, code on top level in hooks gets triggered only on first request
  * In production it gets triggered on app startup
  * https://github.com/sveltejs/kit/issues/927
+ * 
+ * Probably not the best idea, somehow this increases CPU usage of the server and eventually leads to crash
  */
-cleanLogs.start()
-cleanStorage.start()
-cleanCronLogs.start()
+// cleanLogs.start()
+// cleanStorage.start()
+// cleanCronLogs.start()
 
 /**
  * Create a client_secret A.K.A. OAuth web client ID
