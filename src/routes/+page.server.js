653a126e47f2c28084d6f9a19d02579aac6c3ed8
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-04-21 11:58:05 +0800

    [Feature] Convert images to PNG using sharp, allows kmeans_colors unsupported images to be processed

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,11 @@
+import { fail } from '@sveltejs/kit'
 import { writeFile } from 'node:fs/promises'
 import { storage_path } from "$lib/config.js"
 import { kmeansColors } from "$lib/ghostprinter/print-job/pdf.js"
 import { summary } from '$lib/ghostprinter/print-job/cmyk.js'
 import { getOAuth2Client, isSignedIn } from 'svelte-google-auth'
 import { google, Auth, drive_v3 } from 'googleapis'
+import sharp from 'sharp'
 
 /**
  * Sample google server side api call
@@ -145,32 +147,54 @@ export const actions = {
      * 
      * Node Filesystem flags
      * https://nodejs.org/dist/latest-v18.x/docs/api/fs.html#file-system-flags
+     * 
+     * SVG to PNG using Canvas
+     * https://gist.github.com/tatsuyasusukida/1261585e3422da5645a1cbb9cf8813d6
      */
     default: async ({ request, locals, cookies }) => {
-        const formData = await request.formData()
-        const files = formData.getAll('file')
-        const filenames = formData.getAll('filenames')
+        try {
+            const formData = await request.formData()
+            const files = formData.getAll('file')
+            const filenames = formData.getAll('filenames')
 
-        console.log('server files: ', files)
-        console.log('server filenames: ', filenames)
+            console.log('server files: ', files)
+            console.log('server filenames: ', filenames)
 
-        let kmeans_colors = []
+            let kmeans_colors = []
 
-        for (let i = 0; i < files.length; i++) {
-            const buffer = Buffer.from(await files[i].arrayBuffer())
-            const extension = files[i].type.replace('image/', '')
-            const imagepath = `${storage_path}/tmp/file-${i}.${extension}`
-            await writeFile(imagepath, buffer, { flag: 'w+' })
-            const color = await kmeansColors(imagepath)
-            kmeans_colors = [...kmeans_colors, color]
-        }
+            for (let i = 0; i < files.length; i++) {
+                const buffer = Buffer.from(await files[i].arrayBuffer())
 
-        const cmyk = await summary(kmeans_colors)
-        console.log('cmyk: ', cmyk)
+                // const extension = files[i].type.replace('image/', '').replace('+xml', '')
+                // const imagepath = `${storage_path}/tmp/file-${i}.${extension}`
+                // await writeFile(imagepath, buffer, { flag: 'w+' })
 
-        return {
-            kmeans_colors,
-            cmyk
+                const pngBuffer = await sharp(buffer)
+                        .toFormat('png')
+                        .toBuffer()
+                console.log('pngBuffer: ', pngBuffer)
+                const imagepath = `${storage_path}/tmp/file-${i}.png`
+                await writeFile(imagepath, pngBuffer, { flag: 'w+' })
+
+                const color = await kmeansColors(imagepath)
+                kmeans_colors = [...kmeans_colors, color]
+            }
+
+            const cmyk = await summary(kmeans_colors)
+            console.log('cmyk: ', cmyk)
+
+            return {
+                kmeans_colors,
+                cmyk
+            }
+        } catch (error) {
+            error.stderr !== ''
+                ? console.log('Processing image failed: ', error.stderr)
+                : console.log('Processing image failed: ', error.message)
+
+            return error.stderr !== ''
+                ? fail(422, error.stderr)
+                : fail(422, error.message)
         }
     }
 }
