bb32e3b22ea77e44beba8fd263643505d70da9cd
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-05-16 12:59:24 +0800

    Add quirrel as server route, setup as intended by quirrel

---
+++ src/routes/quirrel/+server.js
@@ -0,0 +1,52 @@
+import { Queue } from 'quirrel/sveltekit'
+import { writeFile, appendFile } from 'node:fs/promises'
+import { storage_path } from "$lib/config.js"
+import prisma, { mimetypeMapToEnum, mimetypeMapFromEnum } from '$lib/prisma.js'
+import { getFileExtension } from '$lib/aerial/hybrid/util.js'
+
+/**
+ * This route is dedicated to handling queues added in /api/queue to Quirrel Queue
+ * https://docs.quirrel.dev/api/sveltekit
+ * 
+ * This route endpoint is not supposed to receive requests,
+ * to add items to queue make another endpoint or use a form action to send the request and 
+ * import queue from this file, then call .enqueue method.
+ * 
+ * Sample code
+ * https://github.com/quirrel-dev/quirrel/blob/main/examples/sveltekit/src/routes/greetingsQueue/%2Bserver.ts
+ * 
+ * To see pending queues visit https://ui.quirrel.dev/ and use connect to localhost
+ */
+const queue = Queue(
+    'quirrel',
+    async (artifactCollectionId, meta) => {
+        /**
+         * Perform kmeans_colors and save to database
+         * Get artifact collection instance
+         */
+        const artifactCollection = await prisma.artifactCollection.findFirst({
+            where: {
+                id: artifactCollectionId
+            },
+            include: {
+                artifacts: true
+            }
+        })
+        // test that queue job is invoked
+        await writeFile(`${storage_path}/aerial/${artifactCollection.id}/test.json`, JSON.stringify(artifactCollection, null, 4))
+
+        const artifacts = artifactCollection.artifacts
+
+        artifacts.forEach(async (artifact) => {
+            // test loop
+            // await appendFile(`${storage_path}/aerial/${artifactCollection.id}/artifacts.txt`, artifact.id)
+
+            
+        })
+    }
+)
+
+export const POST = queue
+
+// Error: Invalid export 'default' in src/routes/quirrel/+server.js (valid exports are GET, POST, PATCH, PUT, DELETE, OPTIONS, prerender, trailingSlash, config, or anything with a '_' prefix)
+// export default queue
