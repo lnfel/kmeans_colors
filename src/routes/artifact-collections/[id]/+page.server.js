243da43047cdbd2988a8d005c11c357972f0d429
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-06-09 21:29:11 +0800

    Adjust whitespace

--- src/routes/artifact-collections/[id]/+page.server.js
+++ src/routes/artifact-collections/[id]/+page.server.js
@@ -4,22 +4,22 @@ import { error } from '@sveltejs/kit'
 export const load = async ({ params }) => {
     try {
         console.log(`artifact-collections ID: ${params.id}`)
-    const artifactCollection = await prisma.artifactCollection.findFirstOrThrow({
-        where: {
-            id: params.id
-        },
-        include: {
-            artifacts: {
-                include: {
-                    kmeansColors: true,
-                    cmyk: true
+        const artifactCollection = await prisma.artifactCollection.findFirstOrThrow({
+            where: {
+                id: params.id
+            },
+            include: {
+                artifacts: {
+                    include: {
+                        kmeansColors: true,
+                        cmyk: true
+                    }
                 }
             }
-        }
-    })
+        })
 
-    return {
-        artifactCollection
+        return {
+            artifactCollection
         }
     } catch (errorMessage) {
         throw error(404, 'Artifact collection not found')
