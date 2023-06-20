cf2827e3f01656d6a80e2a3cb8374f5f2cae37fa
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-06-20 17:18:31 +0800

    [Feature] Artifact collections api delete endpoint and handling errors

--- src/routes/artifact-collections/[id]/+server.js
+++ src/routes/artifact-collections/[id]/+server.js
@@ -1,5 +1,7 @@
-import { json } from '@sveltejs/kit'
+import { json, error } from '@sveltejs/kit'
 import prisma, { mimetypeMapToEnum } from '$lib/prisma.js'
+import { rm } from "node:fs/promises"
+import { storage_path } from '$lib/config.js'
 
 /**
  * +server.js file serves as API only endpoint
@@ -19,19 +21,61 @@ import prisma, { mimetypeMapToEnum } from '$lib/prisma.js'
  * @returns {JSON} ArtifactCollection
  */
 export async function GET({ request, url }) {
-    const artifactCollection = await prisma.artifactCollection.findFirstOrThrow({
-        where: {
-            id: url.pathname.replace('/artifact-collections/', '')
-        },
-        include: {
-            artifacts: {
-                include: {
-                    kmeansColors: true,
-                    cmyk: true
+    const id = url.pathname.replace('/artifact-collections/', '')
+    
+    try {
+        const artifactCollection = await prisma.artifactCollection.findFirstOrThrow({
+            where: {
+                id
+            },
+            include: {
+                artifacts: {
+                    include: {
+                        kmeansColors: true,
+                        cmyk: true
+                    }
                 }
             }
-        }
-    })
+        })
 
-    return json(artifactCollection)
+        return json(artifactCollection)
+    } catch (errorMessage) {
+        throw error(404, `Artifact collection with id of ${id} does not exist.`)
+    }
+}
+
+/**
+ * Delete artifact collection data based on id
+ * 
+ * Sample curl request:
+ * curl -inlcude -X DELETE "localhost:5173/artifact-collections/artc_kR8qUu" -H "Accept: application/json"
+ * 
+ * Delete if exist does not exist at the moment, use catch to handle error gracefully
+ * https://github.com/prisma/prisma/issues/4072#issuecomment-1127067981
+ * 
+ * @returns {JSON} ArtifactCollection
+ */
+export async function DELETE({ request, url }) {
+    const id = url.pathname.replace('/artifact-collections/', '')
+
+    try {
+        const deleteArtifacts = prisma.artifact.deleteMany({
+            where: {
+                collectionId: id,
+            },
+        })
+
+        const deleteArtifactCollection = prisma.artifactCollection.delete({
+            where: {
+                id,
+            },
+        })
+
+        const transaction = await prisma.$transaction([deleteArtifacts, deleteArtifactCollection])
+        await rm(`${storage_path}/aerial/${id}`, { force: true, recursive: true })
+
+        return json({ message: `Artifact collection ${id} has been deleted.` })
+    } catch (errorMessage) {
+        throw error(404, `Artifact collection with id of ${id} does not exist.`)
+    }
 }
