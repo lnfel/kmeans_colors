24a868a2f4ce9506ea5dd7c93056192e7733d9cf
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-03 11:49:46 +0800

    [Deprecation] Comment out deprecated code

--- src/lib/ghostprinter/print-job/pdf.js
+++ src/lib/ghostprinter/print-job/pdf.js
@@ -20,26 +20,26 @@ const __dirname = path.dirname(__filename)
  * @param {String} tempdir Custom temporary directory
  * @returns {Object} Ghostprinter PDF object
  */
-export const prepare = async (blob, filename, tempdir) => {
-    console.log('prepare')
-    const extension = blob.type.replace('application/', '')
-    const hash = new ShortUniqueId()()
-    filename = filename ?? `pdf-${hash}`
-    tempdir = tempdir ?? 'storage/tmp/'
-    const filedir = `${tempdir}${filename}`
-    const filepath = `${filedir}.${extension}`
+// export const prepare = async (blob, filename, tempdir) => {
+//     console.log('prepare')
+//     const extension = blob.type.replace('application/', '')
+//     const hash = new ShortUniqueId()()
+//     filename = filename ?? `pdf-${hash}`
+//     tempdir = tempdir ?? 'storage/tmp/'
+//     const filedir = `${tempdir}${filename}`
+//     const filepath = `${filedir}.${extension}`
 
-    return {
-        buffer: Buffer.from(await blob.arrayBuffer()),
-        arrayBuffer: await blob.arrayBuffer(),
-        extension,
-        filename,
-        tempdir,
-        filedir,
-        filepath,
-        hash,
-    }
-}
+//     return {
+//         buffer: Buffer.from(await blob.arrayBuffer()),
+//         arrayBuffer: await blob.arrayBuffer(),
+//         extension,
+//         filename,
+//         tempdir,
+//         filedir,
+//         filepath,
+//         hash,
+//     }
+// }
 
 /**
  * Generate preview for PDF
@@ -50,24 +50,24 @@ export const prepare = async (blob, filename, tempdir) => {
  * @param {Number} scale 
  * @returns {Object} Images array in base64 and kmeans_colors array
  */
-export const preview = async (arrayBuffer, filedir, format = 'image/png', scale = 0.1) => {
-    console.log('preview')
-    const images = await bufferToBase64(arrayBuffer)
-    await saveToTemp(images, filedir)
-    let kmeans_colors = []
+// export const preview = async (arrayBuffer, filedir, format = 'image/png', scale = 0.1) => {
+//     console.log('preview')
+//     const images = await bufferToBase64(arrayBuffer)
+//     await saveToTemp(images, filedir)
+//     let kmeans_colors = []
 
-    console.log('kmeansColors')
-    for (let i = 0; i < images.length; i++) {
-        const imagepath = path.join(__dirname, `../../../../${filedir}/page-${i}.png`)
-        const color = await kmeansColors(imagepath)
-        kmeans_colors = [...kmeans_colors, color]
-    }
+//     console.log('kmeansColors')
+//     for (let i = 0; i < images.length; i++) {
+//         const imagepath = path.join(__dirname, `../../../../${filedir}/page-${i}.png`)
+//         const color = await kmeansColors(imagepath)
+//         kmeans_colors = [...kmeans_colors, color]
+//     }
 
-    return {
-        images,
-        kmeans_colors
-    }
-}
+//     return {
+//         images,
+//         kmeans_colors
+//     }
+// }
 
 /**
  * Calculate dominant colors of image
@@ -80,34 +80,34 @@ export const preview = async (arrayBuffer, filedir, format = 'image/png', scale 
  * @param {String} imagepath Path to image on disk
  * @returns {Promise<Object>} KmeansColor object
  */
-export const kmeansColors = async (imagepath) => {
-    const flags = defaultFlags(imagepath)
-    try {
-        await access(imagepath, constants.R_OK | constants.W_OK)
-    } catch (error) {
-        console.log("Cannot access imagepath.")
-        console.log(error)
-        return error
-    }
-    const {stdout} = await KmeansColors.exec(flags)
-    console.log('KmeansColors stdout: ', stdout)
-    const kmeans = stdout.split('\n')
-    const colors = kmeans[0].split(',')
-    const percentage = kmeans[1].split(',')
-    const color = colors.map((color, index) => {
-        const hexstring = `#${color}`
+// export const kmeansColors = async (imagepath) => {
+//     const flags = defaultFlags(imagepath)
+//     try {
+//         await access(imagepath, constants.R_OK | constants.W_OK)
+//     } catch (error) {
+//         console.log("Cannot access imagepath.")
+//         console.log(error)
+//         return error
+//     }
+//     const {stdout} = await KmeansColors.exec(flags)
+//     console.log('KmeansColors stdout: ', stdout)
+//     const kmeans = stdout.split('\n')
+//     const colors = kmeans[0].split(',')
+//     const percentage = kmeans[1].split(',')
+//     const color = colors.map((color, index) => {
+//         const hexstring = `#${color}`
 
-        return {
-            color: hexstring,
-            hex: hexstring,
-            rgb: hexToRgb(hexstring).join(' '),
-            cmyk: hexToCmyk(hexstring),
-            percentage: (percentage[index] * 100).toFixed(2)
-        }
-    })
+//         return {
+//             color: hexstring,
+//             hex: hexstring,
+//             rgb: hexToRgb(hexstring).join(' '),
+//             cmyk: hexToCmyk(hexstring),
+//             percentage: (percentage[index] * 100).toFixed(2)
+//         }
+//     })
 
-    return color
-}
+//     return color
+// }
 
 /**
  * Convert PDF pages into base64 images
@@ -124,40 +124,40 @@ export const kmeansColors = async (imagepath) => {
  * @param {Number} scale Scale of width and height to be used
  * @returns {Promise<Array>} Images array in Base64
  */
-export const bufferToBase64 = async (arrayBuffer, format = 'image/png', scale = 0.1) => {
-    console.log('bufferToBase64')
-    return new Promise(async (resolve, reject) => {
-        try {
-            const before = Date.now()
+// export const bufferToBase64 = async (arrayBuffer, format = 'image/png', scale = 0.1) => {
+//     console.log('bufferToBase64')
+//     return new Promise(async (resolve, reject) => {
+//         try {
+//             const before = Date.now()
 
-            let images = []
-            const { pdf, pages, viewport } = await pdfJsDocument(arrayBuffer, scale)
+//             let images = []
+//             const { pdf, pages, viewport } = await pdfJsDocument(arrayBuffer, scale)
 
-            // PDFDocumentProxy page starts with 1
-            for (let i = 1; i < pages; i++) {
-                const canvas = createCanvas(viewport.width, viewport.height)
-                const canvasContext = canvas.getContext('2d')
-                const page = await pdf.getPage(i)
+//             // PDFDocumentProxy page starts with 1
+//             for (let i = 1; i < pages; i++) {
+//                 const canvas = createCanvas(viewport.width, viewport.height)
+//                 const canvasContext = canvas.getContext('2d')
+//                 const page = await pdf.getPage(i)
 
-                const renderTask = page.render({
-                    canvasContext,
-                    viewport
-                })
-                await renderTask.promise.then(() => {
-                    return images = [...images, canvas.toDataURL()]
-                })
-            }
+//                 const renderTask = page.render({
+//                     canvasContext,
+//                     viewport
+//                 })
+//                 await renderTask.promise.then(() => {
+//                     return images = [...images, canvas.toDataURL()]
+//                 })
+//             }
 
-            const after = Date.now()
-            console.log(`bufferToBase64 done in ${Math.round((after - before) / 1000)}s`)
+//             const after = Date.now()
+//             console.log(`bufferToBase64 done in ${Math.round((after - before) / 1000)}s`)
 
-            resolve(images)
-        } catch (error) {
-            console.log(error)
-            reject(error)
-        }
-    })
-}
+//             resolve(images)
+//         } catch (error) {
+//             console.log(error)
+//             reject(error)
+//         }
+//     })
+// }
 
 /**
  * Initialize PDF using Mozilla's PDF.js
@@ -166,19 +166,19 @@ export const bufferToBase64 = async (arrayBuffer, format = 'image/png', scale = 
  * @param {Number} scale Scale of width and height to be used
  * @returns {Promise<Object>} pdfJsDocument object
  */
-export const pdfJsDocument = async (arrayBuffer, scale = 0.1) => {
-    console.log('pdfJsDocument')
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker'
-    const loadingTask = pdfjsLib.getDocument(arrayBuffer)
-    const pdf = await loadingTask.promise
-    const firstPage = await pdf.getPage(1)
+// export const pdfJsDocument = async (arrayBuffer, scale = 0.1) => {
+//     console.log('pdfJsDocument')
+//     pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/legacy/build/pdf.worker'
+//     const loadingTask = pdfjsLib.getDocument(arrayBuffer)
+//     const pdf = await loadingTask.promise
+//     const firstPage = await pdf.getPage(1)
 
-    return {
-        pdf,
-        pages: pdf._pdfInfo.numPages,
-        viewport: firstPage.getViewport({ scale })
-    }
-}
+//     return {
+//         pdf,
+//         pages: pdf._pdfInfo.numPages,
+//         viewport: firstPage.getViewport({ scale })
+//     }
+// }
 
 /**
  * Save images to specified directory
@@ -187,21 +187,21 @@ export const pdfJsDocument = async (arrayBuffer, scale = 0.1) => {
  * @param {String} filedir Directory where to save the images
  * @returns {Promise<void>} void
  */
-export const saveToTemp = async (images, filedir) => {
-    console.log('saveToTemp')
-    return new Promise((resolve, reject) => {
-        try {
-            images.forEach(async (image, index) => {
-                const buffer = Buffer.from(image.replace('data:image/png;base64,', ''), 'base64')
-                await writeFile(`${filedir}/page-${index}.png`, buffer, { flag: 'w+' })
-                resolve()
-            })
-        } catch (error) {
-            console.log(error)
-            reject(error)
-        }
-    })
-}
+// export const saveToTemp = async (images, filedir) => {
+//     console.log('saveToTemp')
+//     return new Promise((resolve, reject) => {
+//         try {
+//             images.forEach(async (image, index) => {
+//                 const buffer = Buffer.from(image.replace('data:image/png;base64,', ''), 'base64')
+//                 await writeFile(`${filedir}/page-${index}.png`, buffer, { flag: 'w+' })
+//                 resolve()
+//             })
+//         } catch (error) {
+//             console.log(error)
+//             reject(error)
+//         }
+//     })
+// }
 
 /**
  * Generate preview for PDF using pdf2pic API
@@ -213,64 +213,64 @@ export const saveToTemp = async (images, filedir) => {
  * @param {Object} GhostPrintPDF PDF object using @see{prepare} function
  * @returns {Object} Images array in base64 and kmeans_colors array
  */
-export const pdf2pic = async (GhostPrintPDF) => {
-    const pdfJsDocumentbefore = Date.now()
-    const { pdf, pages, viewport } = await pdfJsDocument(GhostPrintPDF.arrayBuffer, 0.1)
-    const pdfJsDocumentafter = Date.now()
-    console.log(`pdfJsDocument done in ${Math.round((pdfJsDocumentafter - pdfJsDocumentbefore) / 1000)}s`)
+// export const pdf2pic = async (GhostPrintPDF) => {
+//     const pdfJsDocumentbefore = Date.now()
+//     const { pdf, pages, viewport } = await pdfJsDocument(GhostPrintPDF.arrayBuffer, 0.1)
+//     const pdfJsDocumentafter = Date.now()
+//     console.log(`pdfJsDocument done in ${Math.round((pdfJsDocumentafter - pdfJsDocumentbefore) / 1000)}s`)
 
-    const options = {
-        saveFilename: 'page',
-        savePath: GhostPrintPDF.filedir,
-        format: 'png',
-        width: viewport.width,
-        height: viewport.height
-    }
-    console.log('options: ', options)
+//     const options = {
+//         saveFilename: 'page',
+//         savePath: GhostPrintPDF.filedir,
+//         format: 'png',
+//         width: viewport.width,
+//         height: viewport.height
+//     }
+//     console.log('options: ', options)
 
-    const isBase64 = true
-    let images = []
-    const pdf2pic = pdf2picFromBuffer(GhostPrintPDF.buffer, options)
-    console.log('pdf2pic: ', pdf2pic)
-    const pdf2picbefore = Date.now()
-    for (let i = 1; i < pages; i++) {
-        const image = await pdf2pic(i, isBase64)
-        images = [...images, `data:image/png;base64,${image.base64}`]
-    }
-    const pdf2picafter = Date.now()
-    console.log(`pdf2pic done in ${Math.round((pdf2picafter - pdf2picbefore) / 1000)}s`)
+//     const isBase64 = true
+//     let images = []
+//     const pdf2pic = pdf2picFromBuffer(GhostPrintPDF.buffer, options)
+//     console.log('pdf2pic: ', pdf2pic)
+//     const pdf2picbefore = Date.now()
+//     for (let i = 1; i < pages; i++) {
+//         const image = await pdf2pic(i, isBase64)
+//         images = [...images, `data:image/png;base64,${image.base64}`]
+//     }
+//     const pdf2picafter = Date.now()
+//     console.log(`pdf2pic done in ${Math.round((pdf2picafter - pdf2picbefore) / 1000)}s`)
 
-    // Test single instance
-    // const image = await pdf2pic(1, isBase64)
-    // images = [...images, `data:image/png;base64,${image.base64}`]
+//     // Test single instance
+//     // const image = await pdf2pic(1, isBase64)
+//     // images = [...images, `data:image/png;base64,${image.base64}`]
 
-    const saveToTempbefore = Date.now()
-    await saveToTemp(images, GhostPrintPDF.filedir)
-    const saveToTempafter = Date.now()
-    console.log(`saveToFolder done in ${Math.round((saveToTempafter - saveToTempbefore) / 1000)}s`)
+//     const saveToTempbefore = Date.now()
+//     await saveToTemp(images, GhostPrintPDF.filedir)
+//     const saveToTempafter = Date.now()
+//     console.log(`saveToFolder done in ${Math.round((saveToTempafter - saveToTempbefore) / 1000)}s`)
 
-    const kmeansColorsbefore = Date.now()
-    let kmeans_colors = []
+//     const kmeansColorsbefore = Date.now()
+//     let kmeans_colors = []
 
-    console.log('kmeansColors')
-    for (let i = 0; i < images.length; i++) {
-        const imagepath = path.join(__dirname, `../../../../${GhostPrintPDF.filedir}/page-${i}.png`)
-        const color = await kmeansColors(imagepath)
-        kmeans_colors = [...kmeans_colors, color]
-    }
-    const kmeansColorsafter = Date.now()
-    console.log(`kmeansColors done in ${Math.round((kmeansColorsafter - kmeansColorsbefore) / 1000)}s`)
+//     console.log('kmeansColors')
+//     for (let i = 0; i < images.length; i++) {
+//         const imagepath = path.join(__dirname, `../../../../${GhostPrintPDF.filedir}/page-${i}.png`)
+//         const color = await kmeansColors(imagepath)
+//         kmeans_colors = [...kmeans_colors, color]
+//     }
+//     const kmeansColorsafter = Date.now()
+//     console.log(`kmeansColors done in ${Math.round((kmeansColorsafter - kmeansColorsbefore) / 1000)}s`)
 
-    return {
-        images,
-        kmeans_colors
-    }
-}
+//     return {
+//         images,
+//         kmeans_colors
+//     }
+// }
 
 export default {
-    prepare,
-    preview,
-    kmeansColors,
-    bufferToBase64,
-    saveToTemp
+    // prepare,
+    // preview,
+    // kmeansColors,
+    // bufferToBase64,
+    // saveToTemp
 }
