f837f957755c0ba4a4bc1e0c7f85637fb44439d6
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-03 11:36:27 +0800

    [Dependency] Migrate to eslint-plugin-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,29 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	extends: ['eslint:recommended', 'plugin:svelte/recommended'],
+    rules: {
+        "no-extra-parens": 0,
+        "no-unexpected-multiline": 0,
+        "no-unused-vars": ["off", {
+            "args": "none"
+        }],
+        "no-async-promise-executor": 0
+    },
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true
-	}
+	},
+    /**
+     * no-undef complains about globalThis
+     * https://github.com/eslint/eslint/issues/11553
+     */
+    "globals": {
+        "globalThis": false
+    }
 };
