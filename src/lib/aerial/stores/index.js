4e5793756bf2d3ddef4a40fb888e517851477375
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-13 17:35:10 +0800

    Add JSDoc context for pageTransitionsEnabled store

--- src/lib/aerial/stores/index.js
+++ src/lib/aerial/stores/index.js
@@ -1,10 +1,11 @@
 import { writable } from "svelte/store"
 
 /**
+ * User preference for page transition
+ * 
  * @type {import('svelte/store').Writable<?Boolean>} nullable
  */
 export const pageTransitionsEnabled = writable(true)
-
 
 export default {
     pageTransitionsEnabled
