6f155674e3d50de44b59ced2fd1f419e219c573c
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-14 13:12:26 +0800

    Save config comments

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,12 @@
 import * as path from "path"
 import { fileURLToPath } from "url"
 import { STORAGE_PATH } from "$env/static/private"
+// import { platform as currentPlatform } from "os"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export const storage_path = path.join(__dirname, `../../${STORAGE_PATH}`)
+// export const env = process.env.NODE_ENV
+// export const env = import.meta.env.MODE
+// export const platform = currentPlatform()
