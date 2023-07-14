2cceb401d6154c7d4d54a295d2f650f3efe2ee0b
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-14 13:53:34 +0800

    [Change] Update npm scripts to use custom server for rabbitmq and websockets, include quirrel as well

---
+++ aerialProdServer.js
@@ -0,0 +1,31 @@
+import * as path from 'path'
+import * as url from 'url'
+import { createWSSGlobalInstance, onHttpServerUpgrade } from './src/lib/websocket/utils.js'
+import { rabbitCreateGlobalConnection } from './src/lib/rabbitmq/utils.js'
+
+/**
+ * First class support for websockets on sveltekit
+ * https://github.com/suhaildawood/SvelteKit-integrated-WebSocket
+ * 
+ * We run this in production like so:
+ * ORIGIN=http://localhost:3000 PORT=3000 BODY_SIZE_LIMIT=0 node ./aerialProdServer.js
+ * instead of the usual:
+ * ORIGIN=http://localhost:3000 BODY_SIZE_LIMIT=0 node build/index.js
+ */
+const filename = url.fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+// semi-colon is important here since we are using IIAF below
+createWSSGlobalInstance();
+
+// Rabbitmq connect
+rabbitCreateGlobalConnection();
+
+
+/**
+ * Immediately invoked async function (IIAF)
+ */
+(async () => {
+    const { server } = await import(path.resolve(dirname, './build/index.js'))
+    server.server.on('upgrade', onHttpServerUpgrade)
+})()
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -42,17 +42,53 @@ module.exports = {
                 BODY_SIZE_LIMIT: 0
             },
         },
+        {
+            name: "quirrel",
+            script: "DISABLE_TELEMETRY=true quirrel",
+            error_file: "./storage/log/error.log",
+            out_file: "./storage/log/app.log",
+        },
+        {
+            name: "aerial:server",
+            script: "./aerialProdServer.js",
+            error_file: "./storage/log/error.log",
+            out_file: "./storage/log/app.log",
+            env: {
+                NODE_ENV: 'development',
+                PORT: 3000,
+                ORIGIN: "http://localhost:3000",
+                BODY_SIZE_LIMIT: 0
+            },
+            env_production: {
+                NODE_ENV: 'production',
+                PORT: 3000,
+                ORIGIN: "https://www-staging.pingsailor.com",
+                BODY_SIZE_LIMIT: 0
+            },
+        },
+        // {
+        //     name: "websockets",
+        //     script: "server/index.js",
+        //     error_file: "./storage/log/error.log",
+        //     out_file: "./storage/log/app.log",
+        //     env: {
+        //         NODE_ENV: 'production',
+        //         PORT: 3001,
+        //         ORIGIN: "http://localhost:3001",
+        //         BODY_SIZE_LIMIT: 0
+        //     }
+        // },
         /**
          * https://stackoverflow.com/questions/42501219/how-to-make-a-task-job-with-pm2
          */
-        {
-            name: "aerial_cron_jobs",
-            script: "scripts/cron.js",
-            instances: 1,
-            // we already have node-cron handle cron timer
-            // cron_restart: "* * * * *",
-            watch: false,
-            autorestart: false
-        }
+        // {
+        //     name: "aerial_cron_jobs",
+        //     script: "scripts/cron.js",
+        //     instances: 1,
+        //     // we already have node-cron handle cron timer
+        //     // cron_restart: "* * * * *",
+        //     watch: false,
+        //     autorestart: false
+        // }
     ]
 }
