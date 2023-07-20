1234d4c2038290786d18963858f8c90505a5e158
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-20 17:07:14 +0800

    Forgot to commit devLayoutTestEnabled preference store

--- src/lib/aerial/stores/index.js
+++ src/lib/aerial/stores/index.js
@@ -3,9 +3,18 @@ import { writable } from "svelte/store"
 /**
  * User preference for page transition
  * 
+ * @see ../component/Header.svelte for implementation
  * @type {import('svelte/store').Writable<?Boolean>} nullable
  */
 export const pageTransitionsEnabled = writable(true)
+
+/**
+ * Dev preference for testing layout transitions
+ * 
+ * @see ../component/Header.svelte for implementation
+ * @type {import('svelte/store').Writable<?Boolean>} nullable
+ */
+export const devLayoutTestEnabled = writable(true)
 
 export default {
     pageTransitionsEnabled
