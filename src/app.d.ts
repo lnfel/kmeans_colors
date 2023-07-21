03dd0a159e734f070e7e7abce04fd4eaabbfa1c0
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-07-21 17:03:33 +0800

    [Change] Update types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,23 @@
 import { AuthRequest, Auth, Configuration } from "lucia-auth"
 
+// <Auth<Configuration>>
 declare global {
     declare namespace App {
         interface Locals {
-            luciaAuth: AuthRequest<Auth<Configuration>>
+            luciaAuth: AuthRequest
         }
 
-        interface PageData {}
+        interface PageData {
+            user?: import('lucia-auth').UserSchema;
+        }
 
         interface Platform {}
+    }
+    declare namespace Lucia {
+        type Auth = Auth<Configuration>
+        type UserAttributes = {
+            name: string;
+            picture: string;
+        }
     }
 }
