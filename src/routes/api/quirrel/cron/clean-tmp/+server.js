0f8c0368df571cbbd32fe877987b5c95c4f7c1a6
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-08-06 01:40:12 +0800

    Remove unused code

--- src/routes/api/quirrel/cron/clean-tmp/+server.js
+++ src/routes/api/quirrel/cron/clean-tmp/+server.js
@@ -1,9 +1,9 @@
 import { CronJob } from 'quirrel/sveltekit.js'
-import { existsSync, statSync, rmSync } from 'node:fs'
-import { readdir, stat, rm, appendFile } from 'node:fs/promises'
+import { existsSync } from 'node:fs'
+import { readdir, rm, appendFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
 import { storage_path } from '$lib/config.js'
-import { currentDateTime } from '$lib/aerial/hybrid/util.js'
+import { airy, currentDateTime } from '$lib/aerial/hybrid/util.js'
 import { STORAGE_PATH } from '$env/static/private'
 
 /**
@@ -20,7 +20,6 @@ const cron = CronJob(
             const files = await readdir(storageTmpPath)
             files.forEach(async (filename) => {
                 if (filename !== '.gitignore') {
-                    // (await stat(`${storageTmpPath}/${filename}`)).isDirectory()
                     rm(`${storageTmpPath}/${filename}`, { recursive: true, force: true })
                 }
             })
