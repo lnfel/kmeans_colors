4961070c87f1d5e1c9ed89d36bb7314762c98bc7
Dale Ryan Aldover <bk2o1.syndicates@gmail.com>
2023-09-06 12:13:48 +0800

    Code cleaning

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,3 @@
-// import { hydrateAuth } from 'svelte-google-auth/server'
-// import { getOAuth2Client, isSignedIn } from 'svelte-google-auth'
 import { google } from 'googleapis'
 import { listStorageQuota, listAerialFolderDetails } from '$lib/aerial/server/google/drive.js'
 import { luciaAuth, googleAuthConfig } from '$lib/aerial/server/lucia.js'
@@ -11,10 +9,9 @@ import { google_client_secret, app_url, google_oauth_callback_path } from '$lib/
  * @type {import('@sveltejs/kit').ServerLoad}
  */
 export const load = async ({ locals, url, depends }) => {
-    let driveFiles, storageQuota, aerialFolder, googleOauthClient, aerial_token
+    let storageQuota, aerialFolder, googleOauthClient, aerial_token
     const session = await locals.luciaAuth.validate()
-    // airy({ message: locals.luciaAuth, label: '[Layout load] Lucia Auth:' })
-    // airy({ message: session, label: '[Layout load] Session:' })
+
     if (session?.user) {
         const [ authKey ] = (await luciaAuth.getAllUserKeys(session.user.id)).filter((key) => key.providerId === 'google')
         const { access_token, refresh_token, expiry_date, aerial_token: aerialToken } = await prisma.authToken.findFirst({
@@ -23,8 +20,6 @@ export const load = async ({ locals, url, depends }) => {
             }
         })
         aerial_token = aerialToken
-        // airy({ message: authKey, label: '[Layout load] AuthKey:' })
-        // airy({ message: access_token, label: '[Layout load] Access token:' })
         googleOauthClient = new google.auth.OAuth2(
             google_client_secret.web.client_id,
             google_client_secret.web.client_secret,
@@ -34,15 +29,10 @@ export const load = async ({ locals, url, depends }) => {
             access_token,
             refresh_token,
             expiry_date,
-            // id_token,
             token_type: 'Bearer',
             scope: googleAuthConfig.scope.join(' ')
         })
-        // const newToken = await googleOauthClient.getAccessToken()
-        // airy({ message: googleOauthClient, label: '[Layout load] googleOauthClient:' })
-        // airy({ message: newToken, label: '[Layout load] newToken:' })
 
-        // driveFiles = listDriveFiles(googleOauthClient)
         storageQuota = listStorageQuota(googleOauthClient)
         aerialFolder = listAerialFolderDetails(googleOauthClient)
     }
@@ -50,9 +40,6 @@ export const load = async ({ locals, url, depends }) => {
     depends('layout:data')
 
     return {
-        // By calling hydateAuth, certain variables from locals are parsed to the client
-        // allowing the client to access the user information and the client_id for login
-        // ...hydrateAuth(locals),
         // Sveltekit streaming promises for blazingly fast page navigation
         streamed: {
             storageQuota,
